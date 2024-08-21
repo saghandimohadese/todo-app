54c0375807936cef4e570fc2183181be7a34e576
Mohadeseh <saghandimaryam7@gmail.com>
2024-08-21 03:40:03 -0800

    Add remove_todo function & use it on checkbox

--- web.py
+++ web.py
@@ -3,10 +3,17 @@ import modules.functions as functions
 
 todos = functions.get_todos()
 
+
 def add_todo():
     new_todo = st.session_state["new_todo"]
     todos.append(new_todo+"\n")
     functions.set_todos(todos)
+
+
+def remove_todo(todo):
+    todos.remove(todo)
+    functions.set_todos(todos)
+    st.rerun()
 
 
 st.title("My Todo App")
@@ -14,7 +21,9 @@ st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
 for index, todo in enumerate(todos):
-    st.checkbox(todo, key=f"checkbox_{index}")
+    checkbox = st.checkbox(todo, key=f"checkbox_{index}")
+    if checkbox:
+        remove_todo(todo)
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
