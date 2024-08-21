d56253f02466c74c71df3ded46893ffd1723a6b0
Mohadeseh <saghandimaryam7@gmail.com>
2024-08-21 02:58:59 -0800

    Styling gui & add os module to generate todos.txt file & add try except

--- gui.py
+++ gui.py
@@ -1,18 +1,23 @@
 import modules.functions as functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
 
 sg.theme("DarkPurple2")
 
 clock = sg.Text('',  key='clock')
 lebel = sg.Text("type in a to_do")
 input_box = sg.InputText(tooltip="Enter a todo", key="todo")
-add_button = sg.Button("Add", size=14)
+add_button = sg.Button("Add", size=14, mouseover_colors="Green")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 
-complete_button = sg.Button("complete")
+complete_button = sg.Button("Complete")
 
 exit_button = sg.Button("Exit")
 
@@ -27,10 +32,10 @@ window = sg.Window("My To-Do App",
 
 while True:
     event, values = window.read(timeout=20)
-    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
-    print(1, event)
-    print(2, values)
-    print(3, values["todos"])
+
+    # print(1, event)
+    # print(2, values)
+    # print(3, values["todos"])
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -39,27 +44,34 @@ while True:
             functions.set_todos(todos)
             window["todos"].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + "\n"
-            print(todo_to_edit, new_todo)
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.set_todos(todos)
-            window["todos"].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + "\n"
+                print(todo_to_edit, new_todo)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.set_todos(todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                print("Your index is out of range")
         case "todos":
             window["todo"].update(value=values["todos"][0])
-        case "complete":
-            todo_to_complete = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.set_todos(todos)
-            window["todos"].update(values=todos)
+        case "Complete":
+            try:
+                todo_to_complete = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.set_todos(todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                print("Your index is out of range")
         case "Exit":
             break
 
         case sg.WINDOW_CLOSED:
             break
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
 
 
 window.close()
